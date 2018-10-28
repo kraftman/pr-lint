a665a93e5e2292e43a78f8ee39de5e063c213bdf
Chris Tanner <me@itschr.is>
2018-10-27 17:35:05 -0700

    basic docker setup working

--- pr-lint/src/main.go
+++ pr-lint/src/main.go
@@ -1,20 +1,7 @@
 package main
-import (
-     "os"
-     "log"
-     "fmt"
-     "net/http"
-)
-func handler (w http.ResponseWriter, r *http.Request) {
-    log.Println("Received Request: ", r.URL.Path)
-    fmt.Sprintf(w, "Hello World!")
-}
-func main () {
-    http.HandleFunc("/", handler)
-   
-    port := 8080
-    err := http.ListenAndServe(":"+port, nil)
-    if err != nil {
-        log.Fatal("Could not listen: ", err)
-    }
+
+import "fmt"
+
+func main() {
+	fmt.Printf("hello, world\n")
 }
